34a27c0b1c2e17043029042a6db443da14359ebf
edwinsanch33 <edwinsanch33@gmail.com>
2020-09-02 15:01:18 -0400

    Updates

---
+++ precache-manifest.12420ce9f686bc789286d376238b35ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d91bd898837b0dbb81f3e109f1441e",
+    "url": "/Eds-trivia/index.html"
+  },
+  {
+    "revision": "3a2c24841ea1a94e16f1",
+    "url": "/Eds-trivia/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "0edb3a2e67ef1db592ed",
+    "url": "/Eds-trivia/static/css/main.79208805.chunk.css"
+  },
+  {
+    "revision": "3a2c24841ea1a94e16f1",
+    "url": "/Eds-trivia/static/js/2.148d883a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Eds-trivia/static/js/2.148d883a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edb3a2e67ef1db592ed",
+    "url": "/Eds-trivia/static/js/main.325ec357.chunk.js"
+  },
+  {
+    "revision": "005af6a80d1dd4c296d2",
+    "url": "/Eds-trivia/static/js/runtime-main.12c7feef.js"
+  }
+]);
