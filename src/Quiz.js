df378e9d82a8ad2b4ef8c1f5bf8fb90e61b32b0b
edwinsanch33 <edwinsanch33@gmail.com>
2020-10-05 23:14:28 -0400

    add Quiz component to render questions and answers

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
-const Quiz = () => {
+
+const Quiz = (props) => {
+  const {questionInfo, id} = props
+  const {question, incorrect_answers, correct_answer, category} = questionInfo
+  const answers = [...incorrect_answers, correct_answer]
+  console.log(answers)
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
   return (
-    <h1>
-      Hello Quiz!
-    </h1>
+    <div id={id}>
+      <h1>
+        {decodeURIComponent(category)}
+      </h1>
+      <h3>{decodeURIComponent(question)}</h3>
+      {shuffle(answers).map((choice,index) => {
+        return <li value={decodeURIComponent(choice)} key={index}>{decodeURIComponent(choice)}</li>
+      })}
+      
+    </div>
   );
 }
 
